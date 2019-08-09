182f265197dee28b0dbe4ec307e9245433f4fae1
rummolprod999 <rummolprod999@gmail.com>
2019-08-09 14:30:03 +0300

    init

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <?php
+require_once 'templates/header.php';
+?>
+<article>hello world</article>
+<?php
+require_once 'templates/footer.php';
+?>
--- templates/footer.php
+++ templates/footer.php
@@ -1,1 +1,6 @@
-<?php
+<footer id="footer">
+    <small>&copy; rummolprod999</small>
+    <address><a href="mailto:rummolprod999@gmail.com"></a>rummolprod999@gmail.com</address>
+</footer>
+</body>
+</html>
