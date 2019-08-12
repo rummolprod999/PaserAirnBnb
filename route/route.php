f09dd168e1ee0048020df3c2d77d426cc54739ee
rummolprod999 <rummolprod999@gmail.com>
2019-08-12 16:46:03 +0300

    add controllers

--- route/route.php
+++ route/route.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'RouterLite.php';
-if(AuthController::check_auth() || AuthController::$is_login){
+if(AuthController::$is_login || AuthController::check_auth()){
     RouterLite::addRoute('/', 'DefaultController/index_page');
 
 } else{
