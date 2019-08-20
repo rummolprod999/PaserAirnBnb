1c13f0500370fc15939e5383935cf52d57cd432d
rummolprod999 <rummolprod999@gmail.com>
2019-08-20 09:13:32 +0300

    fix issues

--- helpers/calendar.php
+++ helpers/calendar.php
@@ -1,5 +1,82 @@
 <?php
 
-function print_calendar($data){
+function print_calendar($data)
+{
+    ob_start();
+    echo "<div>" . date('F') . "</div>";
+    $dayofmonth = date('t');
 
+    $day_count = 1;
+    $num = 0;
+
+    for ($i = 0; $i < 7; $i++) {
+
+        $dayofweek = date('w',
+
+            mktime(0, 0, 0, date('m'), $day_count, date('Y')));
+
+        $dayofweek = $dayofweek - 1;
+
+        if ($dayofweek == -1) {
+            $dayofweek = 6;
+        }
+
+
+        if ($dayofweek == $i) {
+
+            $week[$num][$i] = $day_count;
+
+            $day_count++;
+
+        } else {
+
+            $week[$num][$i] = "";
+
+        }
+
+    }
+
+    while (true) {
+
+        $num++;
+
+        for ($i = 0; $i < 7; $i++) {
+
+            $week[$num][$i] = $day_count;
+
+            $day_count++;
+
+            if ($day_count > $dayofmonth) break;
+
+        }
+        if ($day_count > $dayofmonth) break;
+
+    }
+
+    echo "<table border=1>";
+    $c = 0;
+    for ($i = 0, $iMax = count($week); $i < $iMax; $i++) {
+
+        echo "<tr>";
+
+        for ($j = 0; $j < 7; $j++) {
+
+            if (!empty($week[$i][$j])) {
+
+                if ($data['days'][$c]['available'] == '1' && $data['days'][$c]['available_for_checkin'] == '1' && $data['days'][$c]['bookable'] == '1')
+
+                    echo "<td><font color=red>" . $week[$i][$j] . "</font></td>";
+
+                else echo "<td>" . $week[$i][$j] . "</td>";
+                $c++;
+            } else echo "<td>&nbsp;</td>";
+
+        }
+
+        echo "</tr>";
+
+    }
+
+    echo "</table>";
+    return ob_get_clean();
 }
--- templates/stat.php
+++ templates/stat.php
@@ -1,4 +1,6 @@
-<?php require_once  'templates/navigation.php'?>
+<?php require_once 'templates/navigation.php' ?>
+<?php require_once 'helpers/calendar.php' ?>
+
 <div><h1 class="text-center">Статистика</h1></div>
 <div id="article">
     <div><strong>Название апартаментов: </strong><?php if (isset($data['info_url']['apartment_name'])) {
@@ -19,83 +21,9 @@
             <div class="py-4 col-xs-12 col-md-8">
                 <div><p><strong>Доступность для заказа</strong></p></div>
                 <?php
-                if (isset($data['days'])):?>
-                    <div><?php echo date('F')?></div>
-                    <?php
-
-                    $dayofmonth = date('t');
-
-                    $day_count = 1;
-                    $num = 0;
-
-                    for ($i = 0; $i < 7; $i++) {
-
-                        $dayofweek = date('w',
-
-                            mktime(0, 0, 0, date('m'), $day_count, date('Y')));
-
-                        $dayofweek = $dayofweek - 1;
-
-                        if ($dayofweek == -1) $dayofweek = 6;
-
-
-                        if ($dayofweek == $i) {
-
-                            $week[$num][$i] = $day_count;
-
-                            $day_count++;
-
-                        } else {
-
-                            $week[$num][$i] = "";
-
-                        }
-
-                    }
-
-                    while (true) {
-
-                        $num++;
-
-                        for ($i = 0; $i < 7; $i++) {
-
-                            $week[$num][$i] = $day_count;
-
-                            $day_count++;
-
-                            if ($day_count > $dayofmonth) break;
-
-                        }
-                        if ($day_count > $dayofmonth) break;
-
-                    }
-
-                    echo "<table border=1>";
-                    $c = 0;
-                    for ($i = 0; $i < count($week); $i++) {
-
-                        echo "<tr>";
-
-                        for ($j = 0; $j < 7; $j++) {
-
-                            if (!empty($week[$i][$j])) {
-
-                                if ($data['days'][$c]['available'] == '1' && $data['days'][$c]['available_for_checkin'] == '1' && $data['days'][$c]['bookable'] == '1')
-
-                                    echo "<td><font color=red>" . $week[$i][$j] . "</font></td>";
-
-                                else echo "<td>" . $week[$i][$j] . "</td>";
-                                $c++;
-                            } else echo "<td>&nbsp;</td>";
-
-                        }
-
-                        echo "</tr>";
-
-                    }
-
-                    echo "</table>"; ?>
-                <?php endif; ?>
+                if (isset($data['days'])) {
+                   echo print_calendar($data);
+                } ?>
             </div>
             <div class="py-4 col-xs-6 col-md-4">
                 <div><p><strong>Цена за минимальный период</strong></p></div>
