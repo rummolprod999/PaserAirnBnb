c52448819bd9b5f81be71b7f06a667702bdad742
rummolprod999 <rummolprod999@gmail.com>
2019-09-17 15:48:03 +0300

    add analitics

--- templates/navigation.php
+++ templates/navigation.php
@@ -1,8 +1,8 @@
 <div id="header">
     <div id="nav">
         <ul>
-            <li><a href="/">Main Page</a></li>
-            <li><a href="/settings">View Logs</a></li>
+            <li><a href="/">Main</a></li>
+            <li><a href="/settings">Logs</a></li>
             <li><a href="/analytics">Analytics</a></li>
             <li></li>
         </ul>
