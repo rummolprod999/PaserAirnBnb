1707046806dd7e384ae67b70290cd4d476b37052
rummolprod999 <rummolprod999@gmail.com>
2019-09-20 11:21:39 +0300

    add analitics

--- models/AnalitycsModel.php
+++ models/AnalitycsModel.php
@@ -12,20 +12,27 @@ class AnalitycsModel extends Model
     public function get_data()
     {
         $data = [];
-        $query = 'SELECT a.start_date, a.end_date FROM analitic a WHERE a.perid_nights = 6 GROUP BY a.start_date, a.end_date ORDER BY a.start_date';
-        $res = $this->conn->query($query)->fetchAll(PDO::FETCH_ASSOC);
-        foreach ($res as $r) {
-            $stmt = $this->conn->prepare('SELECT a.start_date, a.end_date, a.price, au.id, au.own  FROM analitic a JOIN anb_url au on a.id_url = au.id WHERE a.start_date = STR_TO_DATE(:st, \'%Y-%m-%d\') AND a.end_date = STR_TO_DATE(:en, \'%Y-%m-%d\') ORDER BY  a.price ASC');
-            $stmt->bindValue(':st', $r['start_date'], PDO::PARAM_STR);
-            $stmt->bindValue(':en', $r['end_date'], PDO::PARAM_STR);
-            $stmt->execute();
-            $res_inner = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            $periods = [];
-            foreach ($res_inner as $n) {
-                $periods[] = $n;
+        for($i = 6; $i <= 31; $i++){
+            $query = "SELECT a.start_date, a.end_date FROM analitic a WHERE a.perid_nights = {$i} GROUP BY a.start_date, a.end_date ORDER BY a.start_date";
+            $period = [];
+            $res = $this->conn->query($query)->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($res as $r) {
+                $stmt = $this->conn->prepare('SELECT a.start_date, a.end_date, a.price, au.id, au.own  FROM analitic a JOIN anb_url au on a.id_url = au.id WHERE a.start_date = STR_TO_DATE(:st, \'%Y-%m-%d\') AND a.end_date = STR_TO_DATE(:en, \'%Y-%m-%d\') ORDER BY  a.price ASC');
+                $stmt->bindValue(':st', $r['start_date'], PDO::PARAM_STR);
+                $stmt->bindValue(':en', $r['end_date'], PDO::PARAM_STR);
+                $stmt->execute();
+                $res_inner = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                $periods = [];
+                foreach ($res_inner as $n) {
+                    $periods[] = $n;
+                }
+                $period[] = $periods;
             }
-            $data[] = $periods;
+            if(count($period) > 0){
+                $data[] = [$i => $period];
+            }
         }
+
         return $data;
     }
 }
--- templates/analitycs.php
+++ templates/analitycs.php
@@ -1,32 +1,64 @@
 <?php require_once 'templates/navigation.php' ?>
+<?php
+if (!function_exists('array_key_first')) {
+    function array_key_first(array $arr)
+    {
+        foreach ($arr as $key => $unused) {
+            return $key;
+        }
+        return NULL;
+    }
+}
+$tabs = 0;
+$divs = 0;
+?>
 <div id="article">
     <div><h1 class="text-center">ANALITYCS</h1></div>
-    <div id="table_div">
-        <h4 class="text-center">7 DAYS(6 NIGHTS)</h4>
-        <div class="table-responsive">
-            <table class="table table-bordered table-hover">
-                <thead>
-                <tr>
-                    <th>Period</th>
-                    <th>Prices</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php foreach ($data as $d): ?>
-                    <tr>
-                        <td><?php echo "<span class = 'text-info'>{$d[0]['start_date']} - {$d[0]['end_date']}</span>" ?></td>
-                        <td><?php foreach ($d
+    <ul class="nav nav-pills nav-fill" role="tablist">
+        <?php foreach ($data as $m): ?>
+            <li role="presentation" class="nav-item"><a href="#tab<?php echo array_key_first($m) ?>" role="tab"
+                                       data-toggle="tab" class="nav-link<?php if ($tabs === 0) {
+                    echo ' active';
+                } ?>"><?php $tabs++; echo array_key_first($m) ?></a></li>
+        <?php endforeach;?>
+    </ul>
+    <div class="tab-content">
+        <?php foreach ($data as $m): ?>
+            <div role="tabpanel" class="tab-pane <?php if ($divs === 0) {
+                echo 'active';
+            } ?>" id="tab<?php $divs++; echo array_key_first($m) ?>">
+                <div id="table_div">
+                    <h4 class="text-center"><?php echo (int)(array_key_first($m) + 1) ?>
+                        DAYS(<?php echo (int)(array_key_first($m)) ?> NIGHTS)</h4>
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                            <tr>
+                                <th>Period</th>
+                                <th>Prices</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php foreach ($m as $t): ?>
+                                <?php foreach ($t as $k => $d): ?>
+                                    <tr>
+                                        <td><?php echo "<span class = 'text-info'>{$d[0]['start_date']} - {$d[0]['end_date']}</span>" ?></td>
+                                        <td><?php foreach ($d as $p) {
+                                                if ($p['own'] === '1') {
+                                                    echo "<a href='/stat/{$p['id']}' target='_blank'><span class='text-success'>\${$p['price']}</span></a>, ";
+                                                } else {
+                                                    echo "<a href='/stat/{$p['id']}' target='_blank'>\${$p['price']}</a>, ";
+                                                }
+                                            } ?></td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            <?php endforeach; ?>
 
-                                           as $p) {
-                                if ($p['own'] === '1') {
-                                    echo "<a href='/stat/{$p['id']}' target='_blank'><span class='text-success'>\${$p['price']}</span></a>, ";
-                                } else {
-                                    echo "<a href='/stat/{$p['id']}' target='_blank'>\${$p['price']}</a>, ";
-                                }
-                            } ?></td>
-                    </tr>
-                <?php endforeach; ?>
-                </tbody>
-            </table>
-        </div>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach?>
+
     </div>
