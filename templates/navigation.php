fa48c4a841ec5118a1e4aa3ee2719ad41ebd2a45
rummolprod999 <rummolprod999@gmail.com>
2019-09-26 09:36:07 +0300

    reformat

--- templates/navigation.php
+++ templates/navigation.php
@@ -1,5 +1,5 @@
 <div id="header">
-    <div id="nav">
+    <!--<div id="nav">
         <ul>
             <li><a href="/">Main</a></li>
             <li><a href="/settings">Logs</a></li>
@@ -7,5 +7,32 @@
             <li><a href="/analytics2">Analytics2</a></li>
             <li></li>
         </ul>
-    </div>
+    </div>-->
+    <?php
+    function active($currect_page)
+    {
+        $url_array = explode('/', $_SERVER['REQUEST_URI']);
+        $url = end($url_array);
+        if ($currect_page == $url) {
+            echo 'active'; //class name in css
+        }
+    }
+
+    ?>
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link <?php active(''); ?>" href="/">Main</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link <?php active('settings'); ?>" href="/settings">Logs</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link <?php active('analytics'); ?>" href="/analytics">Analytics</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link <?php active('analytics2'); ?>" href="/analytics2">Analytics2</a>
+        </li>
+    </ul>
+
+
 </div>
