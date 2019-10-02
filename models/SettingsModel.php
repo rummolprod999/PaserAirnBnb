f6eae5c11420a4fd84b246e958909f10ef95908b
rummolprod999 <rummolprod999@gmail.com>
2019-10-02 12:08:41 +0300

    reformat

--- models/SettingsModel.php
+++ models/SettingsModel.php
@@ -12,7 +12,7 @@ class SettingsModel extends Model
 
     public function get_data()
     {
-        $stmt = $this->conn->prepare('SELECT c.date_last FROM anb_url a LEFT JOIN checkup c on a.id = c.iid_anb WHERE c.date_last IS NOT NULL LIMIT 1');
+        $stmt = $this->conn->prepare('SELECT c.date_last FROM anb_url a LEFT JOIN checkup c on a.id = c.iid_anb WHERE c.date_last IS NOT NULL ORDER BY c.date_last DESC LIMIT 1');
         $stmt->execute();
         $data = [];
         $data['date_last'] = $stmt->fetch(PDO::FETCH_ASSOC);
