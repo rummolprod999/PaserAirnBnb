2e03efd3de65a81a0b37e566ec57b8b75581cbdc
rummolprod999 <rummolprod999@gmail.com>
2019-10-03 15:32:44 +0300

    change  changes

--- templates/changes.php
+++ templates/changes.php
@@ -25,40 +25,62 @@
             </div>
         </form>
     </div>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="py-4 col-xs-12 col-sm-6">
-                <?php if (isset($data['bookable_changes']) && $data['bookable_changes'] !== null): ?>
-                    <?php /*foreach ($data['bookable_changes'] as $row): */?><!--
-                        <p class="text-danger"><?php /*echo "<span class=\"text-secondary\">{$row['date_parsing']}</span>  NEW BOOKING: {$row['date_cal']}" */?></p>
-                    --><?php /*endforeach; */?>
-                    <?php $arr = group_array($data['bookable_changes']);
-                    foreach ($arr as $k_arr => $v_arr):?>
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-8 border border-5 border-primary rounded mb-3 p-2"><?php foreach ($v_arr as $vv): ?>
-                                        <p class="text-danger"><?php echo "<span class=\"text-secondary\">{$k_arr}</span>  NEW BOOKING: {$vv}" ?></p>
-                                    <?php endforeach; ?></div>
-                                <div class="col-4 align-self-center mb-3 p-1"><?php $c = count($v_arr);
-                                    if ($c === 1) {
-                                        echo "<p class=\"font-weight-bold\">{$c} night</p>";
-                                    } elseif ($c > 1) {
-                                        echo "<p class=\"font-weight-bold\">{$c} nights</p>";
-                                    } ?></div>
+    <ul class="nav nav-pills nav-fill" role="tablist">
+        <li role="presentation" class="nav-item"><a href="#tabbook" role="tab"
+                                                    data-toggle="tab" class="nav-link active">Bookable changes</a></li>
+        <li role="presentation" class="nav-item"><a href="#tabprice" role="tab"
+                                                    data-toggle="tab" class="nav-link">Price changes</a></li>
+    </ul>
+    <div class="tab-content">
+        <div role="tabpanel" class="tab-pane active" id="tabbook">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="py-4 col-12">
+                        <?php if (isset($data['bookable_changes']) && $data['bookable_changes'] !== null): ?>
+                            <?php /*foreach ($data['bookable_changes'] as $row): */ ?><!--
+                        <p class="text-danger"><?php /*echo "<span class=\"text-secondary\">{$row['date_parsing']}</span>  NEW BOOKING: {$row['date_cal']}" */ ?></p>
+                    --><?php /*endforeach; */ ?>
+                            <?php $arr = group_array($data['bookable_changes']);
+                            foreach ($arr as $k_arr => $v_arr):?>
+                                <div class="container-fluid">
+                                    <div class="row">
+                                        <div class="col-md-auto col-8 border border-5 border-primary rounded mb-3 p-2"><?php foreach ($v_arr as $vv): ?>
+                                                <p class="text-danger"><?php echo "<span class=\"text-secondary\">{$k_arr}</span>  NEW BOOKING: {$vv}" ?></p>
+                                            <?php endforeach; ?></div>
+                                        <div class="col-md-auto col-4 align-self-center mb-3 p-1"><?php $c = count($v_arr) - 1;
+                                            if ($c === 1) {
+                                                echo "<p class=\"font-weight-bold\">{$c} night</p>";
+                                            } elseif ($c > 1) {
+                                                echo "<p class=\"font-weight-bold\">{$c} nights</p>";
+                                            } ?></div>
 
-                            </div>
-                        </div>
-                    <?php endforeach; ?>
+                                    </div>
+                                </div>
+                            <?php endforeach; ?>
 
-                <?php endif; ?>
+                        <?php endif; ?>
+                    </div>
+                </div>
             </div>
-            <div class="py-4 col-xs-12 col-sm-6">
-                <?php if (isset($data['price_changes']) && $data['price_changes'] !== null): ?>
-                    <?php foreach ($data['price_changes'] as $row): ?>
-                        <p class="text-danger"><?php echo "<span class=\"text-secondary\">{$row['date_parsing']}</span>  PRICE CHANGE: {$row['date_cal']}   \${$row['price_was']} -> \${$row['price']}" ?></p>
-                    <?php endforeach; ?>
-                <?php endif; ?>
+        </div>
+        <div role="tabpanel" class="tab-pane" id="tabprice">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="py-4 col-12">
+                        <?php if (isset($data['price_changes']) && $data['price_changes'] !== null): ?>
+                            <?php foreach ($data['price_changes'] as $row): ?>
+                                <p class="text-danger"><?php echo "<span class=\"text-secondary\">{$row['date_parsing']}</span>  PRICE CHANGE: {$row['date_cal']}   \${$row['price_was']} -> \${$row['price']}" ?></p>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+    <!--<div class="container-fluid">
+        <div class="row">
+
+
+        </div>
+    </div>-->
 </div>
