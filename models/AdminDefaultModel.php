5f8a68f81eb16d954845169e7a715e2857fc1e3d
rummolprod999 <rummolprod999@gmail.com>
2019-11-11 13:29:37 +0300

    add user

--- models/AdminDefaultModel.php
+++ models/AdminDefaultModel.php
@@ -11,6 +11,12 @@ class AdminDefaultModel extends Model
 
     public function get_data()
     {
+        $add_new_user = $this->add_new_user();
+        if ($add_new_user !== null) {
+            $_SESSION['add_user'] = $add_new_user;
+            header("Location: {$_SERVER['REQUEST_URI']}");
+            exit();
+        }
         $data = [];
         $data['users_list'] = $this->get_users_list();
         return $data;
@@ -24,5 +30,36 @@ class AdminDefaultModel extends Model
             return $res;
         }
         return [];
+    }
+
+    private function add_new_user()
+    {
+        if (isset($_POST['name_user'], $_POST['pass'], $_POST['confirm_pass'], $_POST['proxy_address'], $_POST['proxy_port'], $_POST['proxy_user'], $_POST['proxy_pass']) && $_POST['pass'] === $_POST['confirm_pass']) {
+            $stmt = $this->conn->prepare('SELECT id FROM users WHERE user_name = :user_name');
+            $stmt->bindValue(':user_name', trim($_POST['name_user']), PDO::PARAM_STR);
+            $stmt->execute();
+            if ($stmt->rowCount() > 0) {
+                return "a user with that name exists";
+            }
+            $hash = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+            $stmt = $this->conn->prepare('INSERT INTO users SET user_name = :user_name, user_pass = :user_pass');
+            $stmt->bindValue(':user_pass', $hash, PDO::PARAM_STR);
+            $stmt->bindValue(':user_name', $_POST['name_user'], PDO::PARAM_STR);
+            $stmt->execute();
+            $user_id = $this->conn->lastInsertId();
+            $stmt = $this->conn->prepare('INSERT INTO proxy SET proxy_address = :proxy_address, proxy_port = :proxy_port, proxy_user = :proxy_user, proxy_pass = :proxy_pass, id_user = :user_id');
+            $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+            $stmt->bindValue(':proxy_address', $_POST['proxy_address'], PDO::PARAM_STR);
+            $stmt->bindValue(':proxy_port', (int)$_POST['proxy_port'], PDO::PARAM_INT);
+            $stmt->bindValue(':proxy_user', $_POST['proxy_user'], PDO::PARAM_STR);
+            $stmt->bindValue(':proxy_pass', $_POST['proxy_pass'], PDO::PARAM_STR);
+            $stmt->execute();
+            if ($stmt->rowCount() > 0) {
+                return "new user has been added";
+            } else {
+                return "new user has not been added";
+            }
+        }
+        return null;
     }
 }
--- templates/admin_default.php
+++ templates/admin_default.php
@@ -1,7 +1,10 @@
 <?php require_once 'templates/admin_navigation.php'; ?>
 <div id="article">
     <div><h1 class="text-center">USERS LIST</h1></div>
-
+    <?php if (isset($_SESSION['add_user'])) {
+        echo "<div class='alert alert-success' role='alert'>{$_SESSION['add_user']}</div>";
+        unset($_SESSION['add_user']);
+    } ?>
     <div id="table_div">
         <div class="table-responsive">
             <table class="table table-bordered table-hover">
@@ -28,8 +31,9 @@
                         <td><strong><?php echo $row['proxy_pass'] ?></strong></td>
                         <td><strong><?php echo $row['last_date'] ?></strong></td>
                         <td>
-                            <form class="form-inline" method="get" action="/change_user"><input type="hidden" name="user_id"
-                                                                           value='<?php echo $row['id'] ?>'>
+                            <form class="form-inline" method="get" action="/change_user"><input type="hidden"
+                                                                                                name="user_id"
+                                                                                                value='<?php echo $row['id'] ?>'>
                                 <button type="submit" class="btn btn-warning mb-2">Change</button>
                             </form>
                         </td>
@@ -37,6 +41,59 @@
                 <?php endforeach; ?>
                 </tbody>
             </table>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4 border border-2 border-secondary rounded mb-3 p-2">
+                <div><p><strong>Add new user</strong></p></div>
+                <form class="form" method="post"
+                      oninput='pass.setCustomValidity(pass.value != confirm_pass.value ? "Passwords do not match." : "")'>
+                    <div class="form-group">
+                        <label for="name_user">Name</label>
+                        <input type="text" class="form-control" id="name_user"
+                               placeholder=""
+                               value="" name="name_user" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="pass">Password</label>
+                        <input type="password" class="form-control" id="pass"
+                               placeholder=""
+                               value="" name="pass" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="confirmPass">Confirm password</label>
+                        <input type="password" class="form-control" id="confirmPass"
+                               value="" name="confirm_pass" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="proxyAddr">Proxy address</label>
+                        <input type="text" class="form-control" id="proxyAddr"
+                               placeholder=""
+                               pattern="\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+                               value="" name="proxy_address" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="proxyPort">Proxy port</label>
+                        <input type="text" class="form-control" id="proxyPort"
+                               placeholder="" pattern="\d{1,5}"
+                               value="" name="proxy_port" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="proxyUser">Proxy user</label>
+                        <input type="text" class="form-control" id="proxyUser"
+                               placeholder="" pattern=".+"
+                               value="" name="proxy_user" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="proxyPass">Proxy password</label>
+                        <input type="text" class="form-control" id="proxyPass"
+                               placeholder="" pattern=".+"
+                               value="" name="proxy_pass" required>
+                    </div>
+                    <button type="submit" class="btn btn-success mb-2">Add new user</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
