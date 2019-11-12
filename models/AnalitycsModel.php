5ce1e072448af132a54e569d217abc3652ffb64c
rummolprod999 <rummolprod999@gmail.com>
2019-11-12 12:39:31 +0300

    change analitic

--- models/AnalitycsModel.php
+++ models/AnalitycsModel.php
@@ -13,13 +13,17 @@ class AnalitycsModel extends Model
     {
         $data = [];
         for ($i = 6; $i <= 31; $i++) {
-            $query = "SELECT a.start_date, a.end_date FROM analitic a WHERE a.perid_nights = {$i} GROUP BY a.start_date, a.end_date ORDER BY a.start_date";
+            $query = "SELECT a.start_date, a.end_date FROM analitic a WHERE a.perid_nights = {$i} AND a.id_user = :id_user GROUP BY a.start_date, a.end_date ORDER BY a.start_date";
             $period = [];
-            $res = $this->conn->query($query)->fetchAll(PDO::FETCH_ASSOC);
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindValue(':id_user', AuthController::$uid, PDO::PARAM_INT);
+            $stmt->execute();
+            $res =  $stmt->fetchAll(PDO::FETCH_ASSOC);
             foreach ($res as $r) {
-                $stmt = $this->conn->prepare('SELECT a.start_date, a.end_date, a.price, au.id, au.own  FROM analitic a JOIN anb_url au on a.id_url = au.id WHERE a.start_date = STR_TO_DATE(:st, \'%Y-%m-%d\') AND a.end_date = STR_TO_DATE(:en, \'%Y-%m-%d\') ORDER BY  a.price ASC');
+                $stmt = $this->conn->prepare('SELECT a.start_date, a.end_date, a.price, au.id, au.own  FROM analitic a JOIN anb_url au on a.id_url = au.id WHERE a.start_date = STR_TO_DATE(:st, \'%Y-%m-%d\') AND a.end_date = STR_TO_DATE(:en, \'%Y-%m-%d\') AND a.id_user = :id_user ORDER BY  a.price ASC');
                 $stmt->bindValue(':st', $r['start_date'], PDO::PARAM_STR);
                 $stmt->bindValue(':en', $r['end_date'], PDO::PARAM_STR);
+                $stmt->bindValue(':id_user', AuthController::$uid, PDO::PARAM_INT);
                 $stmt->execute();
                 $res_inner = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 $periods = [];
