670d58735192fba573f560aa22445b68e0aad751
rummolprod999 <rummolprod999@gmail.com>
2019-11-12 15:50:59 +0300

    fix start

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 cd /var/www/html || exit
 echo LANG="ru_RU.UTF-8" >> /etc/default/locale
 echo LC_ALL="ru_RU.UTF-8" >> /etc/default/locale
-usr/bin/python3.5 ./run_anb.py
+/usr/bin/python3.5 ./run_anb.py
