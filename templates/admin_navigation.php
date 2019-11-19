8f1901e0411c8c7bdf8e08e0846c44edd93fddbd
rummolprod999 <rummolprod999@gmail.com>
2019-11-19 11:07:23 +0300

    fix navs

--- templates/admin_navigation.php
+++ templates/admin_navigation.php
@@ -1,13 +1,26 @@
 <div id="header">
-    <ul class="nav nav-tabs">
-        <li class="nav-item">
-            <a class="nav-link <?php active('admin'); ?>" href="/">Main</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link text-danger <?php active('/?action=out'); ?>"
-               href="/?action=out">Logout <?php if (isset($_SESSION['user_name'])) {
-                    echo " {$_SESSION['user_name']}";
-                } ?></a>
-        </li>
-    </ul>
+    <nav class="navbar navbar-expand-sm navbar-light bg-faded">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content"
+                aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="nav-content">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link <?php active('admin'); ?>" href="/">Main</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-danger <?php active('/?action=out'); ?>"
+                       href="/?action=out">Logout</a>
+                </li>
+            </ul>
+
+        </div>
+        <span class="navbar-text justify-content-end">Hello,
+      <?php if (isset($_SESSION['user_name'])) {
+          echo " {$_SESSION['user_name']}";
+      } ?>
+    </span>
+    </nav>
+
 </div>
--- templates/navigation.php
+++ templates/navigation.php
@@ -1,24 +1,35 @@
 <div id="header">
-    <ul class="nav nav-tabs">
-        <li class="nav-item">
-            <a class="nav-link <?php active(''); ?>" href="/">Main</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link <?php active('settings'); ?>" href="/settings">Logs</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link <?php active('analytics'); ?>" href="/analytics">Analytics</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link <?php active('analytics2'); ?>" href="/analytics2">Analytics2</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link text-danger <?php active('/?action=out'); ?>"
-               href="/?action=out">Logout <?php if (isset($_SESSION['user_name'])) {
-                    echo " {$_SESSION['user_name']}";
-                } ?></a>
-        </li>
-    </ul>
+    <nav class="navbar navbar-expand-sm navbar-light bg-faded">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content"
+                aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="nav-content">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link <?php active(''); ?>" href="/">Main</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php active('analytics'); ?>" href="/analytics">Analytics</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php active('analytics2'); ?>" href="/analytics2">Analytics2</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?php active('settings'); ?>" href="/settings">Logs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-danger <?php active('/?action=out'); ?>"
+                       href="/?action=out">Logout</a>
+                </li>
+            </ul>
 
+        </div>
+        <span class="navbar-text justify-content-end">Hello,
+      <?php if (isset($_SESSION['user_name'])) {
+          echo " {$_SESSION['user_name']}";
+      } ?>
+    </span>
+    </nav>
 
 </div>
